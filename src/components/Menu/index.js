dc5b27972c14e4ed42a7037f1ae54dd88aaa75a8
José Algorta <50190637+jopealgorta@users.noreply.github.com>
2023-06-11 01:54:51 +0000

    Add menu text.

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -8,14 +8,20 @@ const Menu = () => {
         <main className='menu'>
             <h2>Our Menu</h2>
             {pizzasData.length > 0 ?
-                <ul className='pizzas'>
-                    {pizzasData.map((pizza, i) =>
-                        <li key={i}>
-                            <Pizza name={pizza.name} ingradients={pizza.ingredients} photoName={pizza.photoName} price={pizza.price} soldOut={pizza.soldOut} />
-                        </li>
-                    )}
-                </ul>
-                : 'No pizzas available!'}
+                <>
+                    <p>
+                        Authentic Italian cuisine. 6 creative dishes to choose from. All
+                        from our stone oven, all organic, all delicious.
+                    </p>
+                    <ul className='pizzas'>
+                        {pizzasData.map((pizza, i) =>
+                            <li key={i}>
+                                <Pizza name={pizza.name} ingradients={pizza.ingredients} photoName={pizza.photoName} price={pizza.price} soldOut={pizza.soldOut} />
+                            </li>
+                        )}
+                    </ul>
+                </>
+                : "We're still working on our menu. Please come back later :)"}
         </main>
     );
 }
